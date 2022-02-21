3d46770191149f78b402a5ee0057236d953e6d8b
Arnau Soler <arnau.soler1@uvic.cat>
2022-02-21 11:21:17 +0100

    5th change to motif

--- seqClass.py
+++ seqClass.py
@@ -26,7 +26,7 @@ else:
 
 if args.motif:
     args.motif = args.motif.upper()
-    print(f'Motif search enabled: looking for motif "{args.motif}" in the sequence "{args.seq}"... ', end = '')
+    print(f'Motif search enabled: looking for motif "{args.motif}" in THEs sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
         print("FOUND")
     else:
